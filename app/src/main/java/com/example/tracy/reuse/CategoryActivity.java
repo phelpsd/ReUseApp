85631d91a4a756e71cc3118af79a1c7eb5fa8162
Dustin Phelps <phelpsd@onid.oregonstate.edu>
2015-06-10 17:09:09 -0500

    Updated front page links

--- app/src/main/java/com/example/tracy/reuse/CategoryActivity.java
+++ app/src/main/java/com/example/tracy/reuse/CategoryActivity.java
@@ -1,11 +1,14 @@
 package com.example.tracy.reuse;
 
 import android.content.Intent;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -19,9 +22,13 @@ import retrofit.client.Response;
 
 public class CategoryActivity extends ActionBarActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
+    TextView webView1;
+    TextView webView2;
+
+    String accept;
+    String pickup;
 
     public static final String ENDPOINT = "http://reuse-group17.appspot.com/";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,18 @@ public class CategoryActivity extends ActionBarActivity implements View.OnClickL
         setContentView(R.layout.activity_main);
 
         getCategories();
+
+        webView1 = (TextView) findViewById(R.id.republicAccept);
+        webView2 = (TextView) findViewById(R.id.republicPickup);
+
+        accept = getResources().getString(R.string.accept_link);
+        pickup = getResources().getString(R.string.pickup_link);
+
+        webView1.setPaintFlags(webView1.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        webView1.setText(accept);
+        webView2.setPaintFlags(webView2.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        webView2.setText(pickup);
+
     }
 
     void getCategories(){
@@ -44,7 +63,7 @@ public class CategoryActivity extends ActionBarActivity implements View.OnClickL
 
 
         //queries the API
-        servAPI.getCategories(new Callback<List<Category>>(){
+        servAPI.getCategories(new Callback<List<Category>>() {
 
 
             @Override
@@ -97,5 +116,28 @@ public class CategoryActivity extends ActionBarActivity implements View.OnClickL
 
         startActivity(itemIntent);
 
+    }
+
+    public void onClick_accept(View v) {
+        if (!accept.startsWith("http://") && !accept.startsWith("https://"))
+            accept = "http://" + accept;
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(accept));
+        try {
+            startActivity(browserIntent);
+        } catch (Exception e) {
+            Intent unrestrictedwebIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(accept));
+            startActivity(unrestrictedwebIntent);
+        }
+    }
+    public void onClick_pickup(View v) {
+        if (!pickup.startsWith("http://") && !pickup.startsWith("https://"))
+            pickup = "http://" + pickup;
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(pickup));
+        try {
+            startActivity(browserIntent);
+        } catch (Exception e) {
+            Intent unrestrictedwebIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(pickup));
+            startActivity(unrestrictedwebIntent);
+        }
     }
 }
