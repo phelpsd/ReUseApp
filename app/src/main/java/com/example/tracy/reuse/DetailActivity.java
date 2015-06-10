526599c3c45556bdd6cef8602755a6265907e251
Dustin Phelps <phelpsd@onid.oregonstate.edu>
2015-06-09 19:41:22 -0500

    Fixed map problems

--- app/src/main/java/com/example/tracy/reuse/DetailActivity.java
+++ app/src/main/java/com/example/tracy/reuse/DetailActivity.java
@@ -2,6 +2,8 @@ package com.example.tracy.reuse;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -9,6 +11,9 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
@@ -76,7 +81,7 @@ public class DetailActivity extends ActionBarActivity {
 
     }
 
-    void getItemsByBus(Business bus){
+    void getItemsByBus(Business bus) {
         //builds a rest adapter
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setLogLevel(RestAdapter.LogLevel.FULL)
@@ -89,15 +94,14 @@ public class DetailActivity extends ActionBarActivity {
         //Category current_cat = new Category(catId, catName);
 
         //queries the API
-        rAPI.getItemsByBusiness(bus, new Callback<List<Item>>(){
+        rAPI.getItemsByBusiness(bus, new Callback<List<Item>>() {
 
             @Override
             public void success(List<Item> items, Response response) {
                 itemsView = (TextView) findViewById(R.id.items);
 
                 itemsView.setText("Items Accepted");
-                for(int i = 0; i < items.size(); i++)
-                {
+                for (int i = 0; i < items.size(); i++) {
                     itemsView.append("\n");
                     itemsView.append(items.get(i).getName());
                 }
@@ -114,26 +118,49 @@ public class DetailActivity extends ActionBarActivity {
         });
 
     }
-    public void onClick_map(View v){
-        String uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?daddr=%f,%f (%s)", 12f, 2f, busAdd);
+
+    public void onClick_map(View v) {
+        String modAdd = busAdd.replace(' ', '+');
+        LatLng p1 = getLocationFromAddress(modAdd);
+        double lat = p1.latitude;
+        double lng = p1.longitude;
+        String uri = String.format(Locale.ENGLISH, "https://www.google.com/maps/place/%s/@%f,%f,13z", modAdd, p1.latitude, p1.longitude);
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
         intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
-        try
-        {
+        try {
             startActivity(intent);
-        }
-        catch(ActivityNotFoundException ex)
-        {
-            try
-            {
+        } catch (ActivityNotFoundException ex) {
+            try {
                 Intent unrestrictedIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
                 startActivity(unrestrictedIntent);
-            }
-            catch(ActivityNotFoundException innerEx)
-            {
+            } catch (ActivityNotFoundException innerEx) {
                 Toast.makeText(this, "Please install a maps application", Toast.LENGTH_LONG).show();
             }
         }
     }
 
+    public LatLng getLocationFromAddress(String strAddress) {
+
+        Geocoder coder = new Geocoder(this);
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            p1 = new LatLng((int) (location.getLatitude() * 1E6),
+                    (int) (location.getLongitude() * 1E6));
+
+            return p1;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
